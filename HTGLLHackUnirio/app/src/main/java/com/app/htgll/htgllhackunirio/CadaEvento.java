19452a88bfa622587c51a3e8b45e7bd8e779d646
LucasSargeir <sargeirolucas@gmail.com>
2018-09-22 12:41:31 -0300

    Terminar ate 23h

---
+++ HTGLLHackUnirio/app/src/main/java/com/app/htgll/htgllhackunirio/CadaEvento.java
@@ -0,0 +1,13 @@
+package com.app.htgll.htgllhackunirio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadaEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cada_eventos);
+    }
+}
